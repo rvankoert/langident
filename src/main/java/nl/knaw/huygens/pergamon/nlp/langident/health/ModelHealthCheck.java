cbd5dee88f95f55e28c5b3e4d807826197e0028d
LB <lhuygi@users.noreply.github.com>
2016-09-07 13:19:40 +0200

    Extend healthcheck

--- src/main/java/nl/knaw/huygens/pergamon/nlp/langident/health/ModelHealthCheck.java
+++ src/main/java/nl/knaw/huygens/pergamon/nlp/langident/health/ModelHealthCheck.java
@@ -1,6 +1,7 @@
 package nl.knaw.huygens.pergamon.nlp.langident.health;
 
 import com.codahale.metrics.health.HealthCheck;
+import com.google.common.base.Optional;
 import nl.knaw.huygens.pergamon.nlp.langident.service.LangIdentResource;
 
 import java.util.Set;
@@ -18,6 +19,12 @@ public class ModelHealthCheck extends HealthCheck {
 
     if (models.isEmpty()) {
       return Result.unhealthy("No models to work with");
+    }
+
+    for (String name : models) {
+      if (resource.listLanguages(Optional.of(name)).isEmpty()) {
+        return Result.unhealthy(String.format("model %s has no languages", name));
+      }
     }
 
     return Result.healthy(String.format("%d models in use: %s", models.size(), models));
